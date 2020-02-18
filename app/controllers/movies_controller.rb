da6f68fb63420ab8469727b695d9610b65d67dde
EC2 Default User <ec2-user@ip-172-31-47-229.us-east-2.compute.internal>
2020-02-18 03:14:51 +0000

    testing

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,5 @@
 class MoviesController < ApplicationController
 
-  def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
-  end
-
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -16,21 +12,20 @@ class MoviesController < ApplicationController
     @checked_ratings.each do |rating|
       params[rating] = true
     end
+
     if params[:sort]
       @movies = Movie.order(params[:sort])
     else
       @movies = Movie.where(:rating => @checked_ratings)
     end
   end
-  
-  
-  
+
   def new
     # default: render 'new' template
   end
 
   def create
-    @movie = Movie.create!(movie_params)
+    @movie = Movie.create!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
@@ -41,7 +36,7 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find params[:id]
-    @movie.update_attributes!(movie_params)
+    @movie.update_attributes!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
@@ -52,14 +47,15 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
-  
-private
-   def check
+
+  private
+
+  def check
     if params[:ratings]
       params[:ratings].keys
     else
       @all_ratings
     end
   end
-  
+
 end
