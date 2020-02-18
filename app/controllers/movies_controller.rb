6ec23cbb27ae5c39b8869d644dfb64adaf168340
EC2 Default User <ec2-user@ip-172-31-47-229.us-east-2.compute.internal>
2020-02-18 09:51:28 +0000

    debug

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,11 +10,32 @@ class MoviesController < ApplicationController
     redirect = false
     ratings = params[:ratings]
 
+    #filter box session
+    if ratings != nil && !ratings.keys.empty?
+      session[:filtered_ratings] = ratings
+      ratings = ratings.keys
+    elsif session[:filtered_ratings] != nil
+      redirect = true
+    else
+      session[:filtered_ratings] = Movie.all_ratings
+      ratings = session[:filtered_ratings]
+    end
+    
+    
+    # sort session
+    sort = params[:sort] != nil ? params[:sort].to_sym : nil
+    if sort == :title || sort == :release_date
+      session[:sorted] = sort
+    elsif session[:sorted] != nil
+      redirect = true
+    end
+    
+
     
     if redirect == true
       redirect_to movies_path(sort: session[:sorted], ratings: session[:filtered_ratings])
     end
-    
+    session.clear
     
     @checked_ratings = checked_boxes
     @checked_ratings.each do |rating|
