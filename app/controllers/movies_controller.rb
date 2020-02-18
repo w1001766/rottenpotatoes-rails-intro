75ce1093977c15ee9dcf21b2472df9b344bab669
EC2 Default User <ec2-user@ip-172-31-47-229.us-east-2.compute.internal>
2020-02-18 08:44:56 +0000

    debug

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -18,15 +18,7 @@ class MoviesController < ApplicationController
     end
    
    
-    if ratings!=nil && !ratings.key.empty?
-      session[params[:ratings]] = ratings
-      ratings = ratings.keys
-    elsif session[params[:ratings]] != nil
-      redirect = true
-    else
-      session[params[:ratings]] = Movie.all_ratings
-      ratings = session[params[:ratings]]
-    end
+    
 
   
     if params[:sort]
