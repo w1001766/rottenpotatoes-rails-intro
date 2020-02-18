29d2a442d9b74e9695e22b47ea8f0d7e9a95c6fd
EC2 Default User <ec2-user@ip-172-31-47-229.us-east-2.compute.internal>
2020-02-18 09:04:38 +0000

    debug

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,9 @@
 class MoviesController < ApplicationController
 
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date, :sort)
+  end
+
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -7,43 +11,50 @@ class MoviesController < ApplicationController
   end
 
   def index
+    @all_ratings = Movie.all_ratings
     redirect = false
+
+    # handle user session for filtes
     ratings = params[:ratings]
-
-    
-    
-    @checked_ratings = checked_boxes
-    @checked_ratings.each do |rating|
-      params[rating] = true
-    end
-   
-    
-    
-
-  
-    if params[:sort]
-      @movies = Movie.order(params[:sort])
+    logger.debug 'Ratings from request: ' + ratings.to_s
+    if ratings != nil && !ratings.keys.empty?
+      session[:filtered_ratings] = ratings
+      ratings = ratings.keys
+    elsif session[:filtered_ratings] != nil
+      redirect = true
     else
-      @movies = Movie.where(:rating => @checked_ratings)
+      session[:filtered_ratings] = Movie.all_ratings
+      ratings = session[:filtered_ratings]
+    end
+
+    # check criterias
+    sort = params[:sort] != nil ? params[:sort].to_sym : nil
+    if sort == :title || sort == :release_date
+      session[:sorted] = sort
+    elsif session[:sorted] != nil
+      redirect = true
+    end
+
+    if redirect == true
+      redirect_to movies_path(sort: session[:sorted], ratings: session[:filtered_ratings])
+    end
+
+    # do search
+    logger.debug 'Ratings => ' + ratings.to_s
+    logger.debug 'Sort => ' + sort.to_s
+    if sort != nil
+      @movies = Movie.where(rating: ratings).order(sort)
+    else
+      @movies = Movie.where(rating: ratings)
     end
   end
-private
 
-  def checked_boxes
-    @all_ratings = Movie.order(:rating).select(:rating).map(&:rating).uniq
-
-    if params[:ratings]
-      params[:ratings].keys
-    else
-      @all_ratings
-    end
-  end
   def new
     # default: render 'new' template
   end
 
   def create
-    @movie = Movie.create!(params[:movie])
+    @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
@@ -54,7 +65,7 @@ private
 
   def update
     @movie = Movie.find params[:id]
-    @movie.update_attributes!(params[:movie])
+    @movie.update_attributes!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
@@ -65,7 +76,5 @@ private
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
-
-  
 
 end
