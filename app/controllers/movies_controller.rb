e87791fe74396e356c72388d7317a3c46f6d2311
EC2 Default User <ec2-user@ip-172-31-47-229.us-east-2.compute.internal>
2020-02-18 21:51:50 +0000

    debug

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -27,17 +27,12 @@ class MoviesController < ApplicationController
     end
     
     
-    sort = params[:sort] != nil ? params[:sort].to_sym : nil
-    if sort == :title || sort == :release_date
-      session[:sorted] = sort
-    elsif session[:sorted] != nil
-      redirect = true
-    end
     
    
     if redirect == true
       redirect_to movies_path(sort: session[:sorted], ratings: session[:filtered])
     end
+    
     
     if params[:sort]
       @movies = Movie.order(params[:sort])
